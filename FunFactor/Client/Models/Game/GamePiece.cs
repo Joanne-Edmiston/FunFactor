24d8d361eee6c32e825afa374152f48cd6d95f99
Joanne Edmiston <joanne.edmiston@gmail.com>
2022-12-28 15:40:54 +0000

    Got pieces dropping

---
+++ FunFactor/Client/Models/Game/GamePiece.cs
@@ -0,0 +1,10 @@
+﻿namespace FunFactor.Client.Models.Game
+{
+    public class GamePiece
+    {
+        public Position Position { get; set; } = new Position();
+        public int Value { get; set; }
+        public bool IsSelected { get; set; }
+
+    }
+}
---
+++ FunFactor/Client/Models/Game/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace FunFactor.Client.Models.Game
+{
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        public bool Matches(Position target)
+        {
+            return X == target.X && Y == target.Y;
+        }
+    }
+}
